0550a891bbefb140896b8a543bff84a30f52c496
frc1511-shared <118783234+frc1511-shared@users.noreply.github.com>
2022-12-04 09:57:27 -0500

    [Charlie] Tank drive

--- src/main/cpp/Controls.cpp
+++ src/main/cpp/Controls.cpp
@@ -15,26 +15,7 @@ void Controls::process() {
 double axisL = controller.GetRawAxis(1);
 double axisR = controller.GetRawAxis(5);
 
-double l = 0;
-double r = 0;
-
- if (axisL > 0.5) {
-    l = 1;
-  } else if (axisL < -0.5) {
-    l = 0;
-  } else {
-    l = 0.5;
-  }
-
-  if (axisR > 0.5) {
-    r = 1;
-  } else if (axisR < -0.5) {
-    r = 0;
-  } else {
-    r = 0.5;
-  }
-
-  drive->tankDrive(r, l);
+drive->tankDrive(axisR, axisL);
 }
 
 void Controls::resetToMode(MatchMode mode) {
